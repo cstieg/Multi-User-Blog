e9828583ad2920d014cad282f9564bfff0d1a52d
cstieg <cstieg4899@yahoo.com>
2017-03-19 17:53:06 +0900

    Make delete post dialog

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function verifyDeletePost(postID) {
+  $('#delete-post-dialog').dialog();
+  $('#delete-post-dialog [name=delete]').attr('onclick', 'deletePost(' + String(postID) + ')');
+}
+
+
+function deletePost(postID) {
+  $.ajax({
+    type : 'POST',
+    url : '/deletepost/' + String(postID),
+    success: function() {
+        $('#' + String(postID)).remove();
+        $("#delete-post-dialog").dialog('close');
+        alert('Post deleted!');
+    }
+  })
+}
+
+function closeDialog() {
+  $('.dialog').dialog('close');
+}
