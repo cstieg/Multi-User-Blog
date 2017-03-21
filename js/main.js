7f734354f47bb64e5d766db908178171b88cc815
cstieg <cstieg4899@yahoo.com>
2017-03-21 18:47:19 +0900

    Unlike functionality

--- js/main.js
+++ js/main.js
@@ -10,16 +10,47 @@ function deletePost(postID) {
     url : '/deletepost/' + String(postID),
     success: function() {
         $('#' + String(postID)).remove();
-        $("#delete-post-dialog").dialog('close');
+        $('#delete-post-dialog').dialog('close');
         alert('Post deleted!');
     }
-  })
+  });
 }
 
 function closeDialog() {
   $('.dialog').dialog('close');
 }
 
-function editPost(postID) {
+function likePost(postID) {
+  $.ajax({
+    type : 'POST',
+    url : '/likepost/' + String(postID),
+    success: function() {
+      // toggle to unlike
+      $('#' + postID + ' .like-button').text = 'Unlike';
 
+      // change onclick to unlike function
+      oldFunc = $('#' + postID + ' .like-button').attr('onclick');
+      newFunc = oldFunc.replace('like', 'unlike');
+      $('#' + postID + ' .like-button').attr('onclick', newFunc);
+
+      // increment like-count
+      likeCount = Integer($('#' + postID + ' .like-count').text);
+      $('#' + postID + ' .like-count').text = likeCount + 1;
+    }
+  });
+}
+
+function unlikePost(postID) {
+  $.ajax({
+    type : 'POST',
+    url : '/unlikepost/' + String(postID),
+    success: function() {
+      // toggle to unlike
+      $('#' + postID + ' .like-button').text = 'Like';
+
+      // increment like-count
+      likeCount = Integer($('#' + postID + ' .like-count').text);
+      $('#' + postID + ' .like-count').text = likeCount - 1;
+    }
+  });
 }
--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import time
 import webapp2
 import jinja2
 import re
+import logging
 from google.appengine.ext import db
 import hashlib
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(autoescape = True, loader = jinja2.FileSystemLoader(template_dir))
+jinja_env = jinja2.Environment(autoescape=True, loader=jinja2.FileSystemLoader(template_dir))
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -21,24 +22,30 @@ class Handler(webapp2.RequestHandler):
         self.write(self.render_str(template, **kw))
 
 class BlogEntry(db.Model):
-    title = db.StringProperty(required = True)
-    entry = db.TextProperty(required = True)
-    author = db.TextProperty(required = True)
-    posted = db.DateTimeProperty(auto_now_add = True)
-    likes = db.IntegerProperty(default = 0)
-    comments = db.IntegerProperty(default = 0)
+    title = db.StringProperty(required=True, indexed=True)
+    entry = db.TextProperty(required=True)
+    author = db.StringProperty(required=True, indexed=True)
+    posted = db.DateTimeProperty(auto_now_add=True, indexed=True)
+    likes = db.IntegerProperty(default=0, indexed=True)
+    comments = db.IntegerProperty(default=0, indexed=True)
 
 class Comment(db.Model):
-    blogEntryID = db.IntegerProperty(required = True)
-    comment = db.TextProperty(required = True)
-    author = db.TextProperty(required = True)
-    posted = db.DateTimeProperty(auto_now_add = True)
+    blogEntryID = db.IntegerProperty(required=True, indexed=True)
+    comment = db.TextProperty(required=True)
+    author = db.StringProperty(required=True, indexed=True)
+    posted = db.DateTimeProperty(auto_now_add=True, indexed=True)
     likes = db.IntegerProperty()
+    
+class PostLike(db.Model):
+    blogEntryID = db.IntegerProperty(required=True, indexed=True)
+    liker = db.StringProperty(required=True, indexed=True)
+    likeTime = db.DateTimeProperty(auto_now_add=True)
+    
 
 class User(db.Model):
-    username = db.StringProperty(required = True)
-    password = db.StringProperty(required = True)
-    signedUp = db.DateTimeProperty(auto_now_add = True)
+    username = db.StringProperty(required=True, indexed=True)
+    password = db.StringProperty(required=True, indexed=True)
+    signedUp = db.DateTimeProperty(auto_now_add=True, indexed=True)
 
 class MainPage(Handler):
     def get(self, q=""):       
@@ -50,6 +57,20 @@ class MainPage(Handler):
             blogEntries = [db.get(entryKey)]
         else:
             blogEntries = db.GqlQuery("SELECT* FROM BlogEntry ORDER BY posted DESC")
+    
+        # recreate query in list of objects in order to be able to pass in 'liked' variable    
+        userName = getUsername(self)
+        if userName:
+            entryObjects = []
+            for blogEntry in blogEntries:
+                entryObject = {}
+                for field in blogEntry.properties():
+                    entryObject[field] = getattr(blogEntry, field)
+                entryObject['id'] = blogEntry.key().id() 
+                entryObject['liked'] = postIsLiked(blogEntry, userName)
+                entryObjects.append(entryObject)
+            blogEntries = entryObjects
+        
         self.render("mainpage.html", blogEntries=blogEntries, username=getUsername(self))
         
 class Compose(Handler):
@@ -154,8 +175,66 @@ class EditPost(Handler):
                 self.error(401)
         else:
             # if post id not found, it is a bad request
-            self.error(400)        
+            self.error(400)
+            
+class LikePost(Handler):
+    """Adds a user like and increments like count for post"""
+    def post(self, q=""):
+        # check to make sure valid login
+        if not validUserLogin(self):
+            self.redirect("/login")
+        if q:
+            # query post by id passed in
+            entryKey = db.Key.from_path('BlogEntry', int(q))
+            
+            if not entryKey:
+                self.error(400)
+                return
+            blogEntry = db.get(entryKey)
+            
+            # author cannot like
+            liker = getUsername(self)
+            if liker == blogEntry.author:
+                self.error(401)
+                return
+            
+            if postIsLiked(blogEntry, liker):
+                # forbidden to like twice
+                self.error(403)
+                return
+            
+            likePost(blogEntry, liker)
+            
+        else:
+            self.error(400)
+          
+                
+class UnlikePost(Handler):
+    """Cancels a user like and decrements like count for post"""
+    def post(self, q=""):
+        # check to make sure valid login
+        if not validUserLogin(self):
+            self.redirect("/login")
+        if q:
+            # query post by id passed in
+            entryKey = db.Key.from_path('BlogEntry', int(q))
+            
+            if not entryKey:
+                self.error(400)
+                return
+            blogEntry = db.get(entryKey)
 
+            liker = getUsername(self)
+            if not postIsLiked(blogEntry, liker):
+                # cannot unlike if not liked
+                self.error(403)
+                return
+             
+            unlikePost(blogEntry, liker)
+            
+        else:
+            self.error(400)
+            
 class Signup(Handler):
     def get(self):
         self.render("signup.html")
@@ -166,7 +245,7 @@ class Signup(Handler):
         verify = self.request.get("verify")
         email = self.request.get("email")
 
-        #check inputs against regex
+        # check inputs against regex
         username_re = re.compile("^[a-zA-z0-9_-]{3,20}$")
         username_invalid = not username_re.match(username)
 
@@ -181,26 +260,26 @@ class Signup(Handler):
         invalid_input = username_invalid or password_invalid or verify_invalid or email_invalid
 
         if invalid_input:
-            self.render("signup.html", username_invalid = username_invalid,
-                                    password_invalid = password_invalid,
-                                    verify_invalid = verify_invalid,
-                                    email_invalid = email_invalid,
-                                    invalid_input = invalid_input,
-                                    old_username = username,
-                                    old_email = email)
+            self.render("signup.html", username_invalid=username_invalid,
+                                    password_invalid=password_invalid,
+                                    verify_invalid=verify_invalid,
+                                    email_invalid=email_invalid,
+                                    invalid_input=invalid_input,
+                                    old_username=username,
+                                    old_email=email)
         else:
             usersWithSameName = db.GqlQuery("SELECT * FROM User WHERE username = '%s'" % username)
             if usersWithSameName.count() > 0:
                 
-                self.render("signup.html",  username_already_taken = True,
-                                            old_username = username,
-                                            old_email = email)
+                self.render("signup.html", username_already_taken=True,
+                                            old_username=username,
+                                            old_email=email)
             else:
                 newUser = User(username=username, password=password)
                 newUser.put()
-                self.response.set_cookie('username', username, max_age=60*60*24)
-                self.response.redirect('/newpost')
-                self.render("success.html", username = username)
+                self.response.set_cookie('username', username, max_age=60 * 60 * 24)
+                self.redirect('/newpost')
+                self.render("success.html", username=username)
 
 
 class Login(Handler):
@@ -217,12 +296,12 @@ class Login(Handler):
         if isValidUser(username, password):
             # success, redirect to calling page
             logout(self)
-            self.response.set_cookie('username', username, max_age=60*60*24)
+            self.response.set_cookie('username', username, max_age=60 * 60 * 24)
             self.response.set_cookie('password', password)
             self.redirect("/" + caller)
 
         else:
-            self.render("login.html", login_error = True, caller=caller)
+            self.render("login.html", login_error=True, caller=caller)
 
 class Logout(Handler):
     def get(self):
@@ -236,7 +315,6 @@ def validUserLogin(handler):
 
 def isValidUser(username, password):
     return username and password and db.GqlQuery("SELECT * FROM User WHERE username ='%s' AND password = '%s'" % (username, password)).count() == 1
-
 
 def getUsername(handler):
     if validUserLogin(handler):
@@ -258,6 +336,33 @@ def saltedHash(text, salt):
 def saltedHexString(text, salt):
     return saltedHash(text, salt) + "," + salt
 
+@db.transactional(xg=True)
+def likePost(blogEntry, liker):
+    """Adds a PostLike entity and increments the like count for the BlogEntry entity"""
+    newLike = PostLike(parent=blogEntry, blogEntryID=blogEntry.key().id(), liker=liker)
+    newLike.put()
+    
+    blogEntry.likes += 1
+    blogEntry.put()
+    
+@db.transactional(xg=True)
+def unlikePost(blogEntry, unliker):
+    """Deletes a PostLike entity and decrements the like count for the BlogEntry entity"""
+    likesQuery = PostLike.all()
+    likesQuery.ancestor(blogEntry)
+    likesQuery.filter('liker =', unliker)
+    newLike = likesQuery.get()
+    newLike.delete()
+    
+    blogEntry.likes -= 1
+    blogEntry.put()
+    
+
+def postIsLiked(blogEntry, likerID):
+    blogEntryID = str(blogEntry.key().id())
+    likesQuery = db.GqlQuery("SELECT* FROM PostLike WHERE blogEntryID = %s AND liker = '%s'" % (blogEntryID, likerID))
+    return (likesQuery.count() > 0)
+    
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/([0-9]+)', MainPage),
                                ('/newpost', Compose),
@@ -265,7 +370,9 @@ app = webapp2.WSGIApplication([('/', MainPage),
                                ('/login', Login),
                                ('/logout', Logout),
                                ('/deletepost/([0-9]+)', DeletePost),
-                               ('/editpost/([0-9]+)', EditPost)],
-                                debug=True)
+                               ('/editpost/([0-9]+)', EditPost),
+                               ('/likepost/([0-9]+)', LikePost),
+                               ('/unlikepost/([0-9]+)', UnlikePost)
+                               ], debug=True)
 
 
